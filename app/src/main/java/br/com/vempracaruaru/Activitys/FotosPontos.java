6efbd5e71c84f656acfb47dfc933174b8a1016c1
flavio <flaviosallles@hotmail.com>
2016-03-29 14:54:11 -0300

    Telas Pontos Turisticos

---
+++ app/src/main/java/br/com/vempracaruaru/Activitys/FotosPontos.java
@@ -0,0 +1,15 @@
+package br.com.vempracaruaru.Activitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.joao.vempracaruaruapp.R;
+
+public class FotosPontos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fotos_pontos_layout);
+    }
+}
