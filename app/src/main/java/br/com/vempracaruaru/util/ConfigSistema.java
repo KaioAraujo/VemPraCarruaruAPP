1585bfb8653cf4fc3d13a47013c2678ca4b2e2ca
KaioAraujo <kaio_web@hotmail.com>
2016-04-04 18:37:39 -0300

    Web Serviçe OK

--- app/src/main/java/br/com/vempracaruaru/util/ConfigSistema.java
+++ app/src/main/java/br/com/vempracaruaru/util/ConfigSistema.java
@@ -2,5 +2,5 @@ package br.com.vempracaruaru.util;
 
 public class ConfigSistema {
 //    public static final String URL = "http://192.168.1.104:8080/VemPraCaruaru/Android";
-    public static final String URL = "http://vempracaruaru.com.br:8080/Android";
+    public static final String URL = "http://vempracaruaru.com.br/SIS/Android";
 }
